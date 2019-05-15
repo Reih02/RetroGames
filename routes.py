69a4bea053ddc4776c27cf7250c0038ae0bd4fb9
16059 Reilly Haskins <16059@burnside.school.nz>
2019-05-15 14:21:13 +1200

    Improved the display of elements from database with CSS

--- routes.py
+++ routes.py
@@ -3,9 +3,11 @@ import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html", page_title="WELCOME TO RETRO GAMES")
+
 
 @app.route('/games')
 def navigation():
@@ -16,6 +18,7 @@ def navigation():
     return render_template("list_games.html", page_title="RETRO GAMES",
                            results=results)
 
+
 @app.route('/games/<game>')
 def game(game):
     conn = sqlite3.connect("retro_games.db")
@@ -24,11 +27,13 @@ def game(game):
                 .format(game))
     results = cur.fetchone()
     return render_template("show_games.html", page_title='{}'.format(game),
-                       results=results)
+                           results=results)
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html", page_title="CONTACT US")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=1111)
